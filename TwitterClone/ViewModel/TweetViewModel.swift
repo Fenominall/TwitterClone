422e6730e40a9ca6450fae757eeb755b8dfd0694
Vladislav Todorov <it.vtodorov@gmail.com>
2021-12-20 22:29:56 +0200

    Tweet TimeStamp

--- TwitterClone/ViewModel/TweetViewModel.swift
+++ TwitterClone/ViewModel/TweetViewModel.swift
@@ -17,10 +17,24 @@ struct TweetViewModel {
         return user.profileImageUrl
     }
     
+    var timeStamp: String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.second, .minute, .hour, .day, .weekOfMonth]
+        formatter.maximumUnitCount = 1
+        formatter.unitsStyle = .abbreviated
+        let timeNow = Date()
+        return formatter.string(from: tweet.timestamp, to: timeNow) ?? "2m"
+        
+        
+    }
+    
     var userInfoText: NSAttributedString {
         let title = NSMutableAttributedString(string: user.fullname,
                                               attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
         title.append(NSAttributedString(string: " @\(user.username)",
+                                        attributes: [.font: UIFont.systemFont(ofSize: 14),
+                                                     .foregroundColor: UIColor.lightGray]))
+        title.append(NSAttributedString(string: "・\(timeStamp)",
                                         attributes: [.font: UIFont.systemFont(ofSize: 14),
                                                      .foregroundColor: UIColor.lightGray]))
         return title
