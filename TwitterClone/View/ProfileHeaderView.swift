558c2855f444e8d6c34ce8cb3519ccf0e5fc6447
Vladislav Todorov <it.vtodorov@gmail.com>
2021-12-21 22:37:52 +0200

    profile header UI

--- TwitterClone/View/ProfileHeaderView.swift
+++ TwitterClone/View/ProfileHeaderView.swift
@@ -24,14 +24,57 @@ class ProfileHeaderView: UICollectionReusableView {
     private lazy var backButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(Constants.backButtonImage, for: .normal)
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
         return button
     }()
     
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .lightGray
+        imageView.layer.borderWidth = 4
+        imageView.layer.borderColor = UIColor.white.cgColor
+        return imageView
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Loading", for: .normal)
+        button.layer.borderColor = UIColor.twitterBlue?.cgColor
+        button.layer.borderWidth = 1.25
+        button.setTitleColor(.twitterBlue, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleEditProfileFollow), for: .touchUpInside)
+        return button
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.text = "Eddie Brock"
+        return label
+    }()
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .lightGray
+        label.text = "@venom"
+        return label
+    }()
+    
+    private let bioLableL: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 3
+        label.text = "init(coder:) has not been implemented init(coder:) has not been implemented init(coder:) has not been implemented"
+        return label
+    }()
     
     // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
@@ -41,9 +84,52 @@ class ProfileHeaderView: UICollectionReusableView {
     
     
     // MARK: - Selectors
+    @objc func handleDismissal() {
+        
+    }
     
+    @objc func handleEditProfileFollow() {
+        
+    }
     // MARK: - Helpers
-    
+    func configureUI() {
+
+        addSubview(container)
+        container.anchor(top: topAnchor,
+                         left: leftAnchor,
+                         right: rightAnchor,
+                         height: 108)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: container.bottomAnchor,
+                                left: leftAnchor,
+                                paddingTop: -24, paddingLeft: 8)
+        profileImageView.setDimensions(width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: container.bottomAnchor,
+                                       right: rightAnchor,
+                                       paddingTop: 12, paddingRight: 12)
+        editProfileFollowButton.setDimensions(width: 100, height: 36)
+        editProfileFollowButton.layer.cornerRadius = 36 / 2
+        
+        let userDetailStack  = UIStackView(arrangedSubviews: [fullnameLabel,
+                                                              usernameLabel,
+                                                              bioLableL])
+        userDetailStack.axis = .vertical
+        userDetailStack.distribution = .fill
+        userDetailStack.spacing = 4
+        
+        addSubview(userDetailStack)
+        
+        userDetailStack.anchor(top: profileImageView.bottomAnchor,
+                               left: leftAnchor,
+                               right: rightAnchor,
+                               paddingTop: 12,
+                               paddingLeft: 12,
+                               paddingRight: 12)
+    }
     
     
 }
