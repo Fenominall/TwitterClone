38afda4ba85daa22680f8f76466de1ff8cae2154
Vladislav Todorov <it.vtodorov@gmail.com>
2022-01-08 21:06:38 +0200

    Action Sheet Cell  Cancel Button

--- TwitterClone/Utilities/ActionSheetLauncher.swift
+++ TwitterClone/Utilities/ActionSheetLauncher.swift
@@ -28,6 +28,33 @@ class ActionSheetLauncher: NSObject {
         return view
     }()
     
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemGroupedBackground
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var footerView: UIView = {
+        let view = UIView()
+        
+        view.addSubview(cancelButton)
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.anchor(left: view.leftAnchor,
+                            right: view.rightAnchor,
+                            paddingLeft: 12,
+                            paddingRight: 12)
+        
+        cancelButton.centerY(inView: view)
+        cancelButton.layer.cornerRadius = 50 / 2
+        
+        return view
+    }()
+
     // MARK: - Lifecycle
     init(user: User) {
         self.user = user
@@ -54,29 +81,31 @@ class ActionSheetLauncher: NSObject {
         window.addSubview(blackView)
         blackView.frame = window.frame
         window.addSubview(tableView)
+        // Defining tableView height for three elements
+        let height = CGFloat(3 * 60) + 100
         tableView.frame = CGRect(x: 0,
                                  // set the black view to be the entire screen
                                  y: window.frame.height,
                                  width: window.frame.width,
-                                 height: 300)
+                                 height: height)
         // Animation to smoothly move tableView up on the set height in y
         UIView.animate(withDuration: 0.5) {
             self.blackView.alpha = 1
             // with the animation table view will be moved up
-            self.tableView.frame.origin.y -= 300
+            self.tableView.frame.origin.y -= height
         }
     }
     
     func configureTableView() {
-        tableView.backgroundColor = .red
+        tableView.backgroundColor = .white
         tableView.delegate = self
         tableView.dataSource = self
         
         tableView.rowHeight = 60
         tableView.separatorStyle = .none
-        tableView.layer.cornerRadius = 5
+        tableView.layer.cornerRadius = 7
         tableView.isScrollEnabled = false
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.register(ActionSheetCell.self, forCellReuseIdentifier: reuseIdentifier)
         
     }
 }
@@ -88,7 +117,7 @@ extension ActionSheetLauncher: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ActionSheetCell
         return cell
     }
 }
@@ -96,5 +125,11 @@ extension ActionSheetLauncher: UITableViewDataSource {
 // MARK: - ActionSheetLauncher UITableViewDelegate
 
 extension ActionSheetLauncher: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return footerView
+    }
     
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 60
+    }
 }
---
+++ TwitterClone/View/ActionSheetCell.swift
@@ -0,0 +1,52 @@
+//
+//  ActionSheetCell.swift
+//  TwitterClone
+//
+//  Created by Fenominall on 1/8/22.
+//
+
+import UIKit
+
+class ActionSheetCell: UITableViewCell {
+    
+    // MARK: - Properties
+    private let optionImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = Constants.twitterImageBlue
+        imageView.setDimensions(width: 36, height: 36)
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = "Test Option"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(optionImageView)
+        optionImageView.centerY(inView: self)
+        optionImageView.anchor(left: leftAnchor, paddingLeft: 8)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: self)
+        titleLabel.anchor(left: optionImageView.rightAnchor, paddingLeft: 12)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
